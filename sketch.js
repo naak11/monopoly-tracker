cacf6723cee6840810079fb71657606e67aa9594
Naheek <65249340+naak11@users.noreply.github.com>
2021-07-21 00:01:42 +0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,6 +22,7 @@ let undo2;
 function setup() {
     createCanvas(400, 400);
     let pr = window.prompt('starting number');
+    if(!pr){pr = 0}
     p1 = float(pr);
     p2 = float(pr);
     p1p = p1;
